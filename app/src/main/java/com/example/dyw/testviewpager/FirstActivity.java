8a354c07be6270feb9ec932c2a6f91db143b7b4b
戴亚伟 <dyw19951201@gmail.com>
2019-08-14 13:59:48 +0800

    测试提交

--- app/src/main/java/com/example/dyw/testviewpager/FirstActivity.java
+++ app/src/main/java/com/example/dyw/testviewpager/FirstActivity.java
@@ -9,5 +9,6 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+	Log.d("a","b");
     }
 }
